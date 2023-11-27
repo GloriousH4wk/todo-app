c02ad03639f3b07b896e9859ab67f1dbdf6b6d2f
Kaan <kagan.hawk.aka@gmail.com>
2023-11-27 15:57:14 +0100

    Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoApp';
+  user = 'Kaan';
+  items = [
+    { description: 'Buy groceries for Breakfast', action: 'no' },
+    { description: 'Buy cinema tickets', action: 'no' },
+    { description: 'Go to the gym', action: 'no' },
+    { description: 'write a invoice', action: 'no' },
+  ];
 }
